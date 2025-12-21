11a40140270651322947b4cccf1550d3e80f7958
ANKIT KUMAR <ankitmom0393@gmail.com>
2025-12-21 21:43:12 +0530

    build routes middlware in server.js

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", require("./src/routes/auth"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
