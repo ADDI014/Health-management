15fef4c4ab56cb287212d35fb50851ddc4762815
ANKIT KUMAR <ankitmom0393@gmail.com>
2025-12-21 18:44:36 +0530

    setup project and make connection to mongoDB

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./src/config/db");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`server ruuning on port ${PORT}`);
+});
+
