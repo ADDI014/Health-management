c3041c3b0215d9f5ea8a243f2a9347e677c790d3
ANKIT KUMAR <ankitmom0393@gmail.com>
2025-12-21 22:05:29 +0530

    build logic for health data

--- backend/src/controllers/health.js
+++ backend/src/controllers/health.js
@@ -1,2 +1,27 @@
 
 const HealthRecord = require("../models/HealthRecord");
+
+const {generateHealthResult} = require("../utils/mockAI");
+
+exports.submitHealth = async (req,res) => {
+    try {
+        const {sleep , stress, activity, appetite} = req.body;
+
+        if(!sleep || !stress || !activity) {
+            return res.status(400).json({message : "All fields required"});
+        }
+
+        const result = generateHealthResult({sleep , stress, activity});
+
+        const record = await HealthRecord.create({
+            userId : req.user.id,
+            inputData : req.body,
+            result
+        });
+
+        res.status(201).json(record);
+    }
+    catch {
+        res.status(500).json({message : "Health submission failed"});
+    }
+}
