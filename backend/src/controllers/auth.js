798b43d8d7b02b764c7adc53d721b5f894ae224c
ANKIT KUMAR <ankitmom0393@gmail.com>
2025-12-21 21:04:50 +0530

    build logic for login user

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -29,3 +29,36 @@ exports.register = async (req, res) => {
     res.status(500).json({message : "Registered failed"});
   }
 };
+
+
+exports.login = async (req,res) => {
+    try {
+        const {email , password } = req.body;
+
+        if(!email || !password) {
+            return res.status(400).json({message : "All fields required"});
+        }
+
+        const user = await User.findOne({email});
+
+        if(!user) {
+            return res.status(400).json({message : "Invalid credentials"});
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) {
+            return res.status(400).json({message : "Invalid credentials"});
+        }
+
+        const token = jwt.sign({id : user._id} , process.env.JWT_SECRET, {
+            expiresIn : "1d",
+        });
+
+        res.json({
+            token,
+            user : {id : user._id, name : user.name, email : user.email}
+        });
+    }
+    catch{
+        res.status(500).json({message : "Login failed"});
+    }
+}
