edd47d894199a7ab7797dce7e1e67e8166c1b96f
ANKIT KUMAR <ankitmom0393@gmail.com>
2025-12-23 22:15:36 +0530

    Build Login page and add route in app.jsx

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,42 @@
+import { motion } from "framer-motion";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { useState } from "react";
+import api from "../services/api";
+import toast from "react-hot-toast";
+
+export default function Login(){
+
+    const {login} = useContext(AuthContext);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const submit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const res = await api.post("/auth/login", {email, password});
+            login(res.data.user, res.data.token);
+            toast.success("Login successfull");
+        }
+        catch(error) {
+            toast.error(error.response?.data?.message || "Login failed");
+        }
+    }
+    
+    
+    return (
+    <motion.div
+      initial={{ opacity: 0, y: 40 }}
+      animate={{ opacity: 1, y: 0 }}
+      className="min-h-screen flex items-center justify-center bg-gray-100"
+    >
+      <form onSubmit={submit} className="card w-96">
+        <h2 className="text-2xl font-bold mb-4 text-center">Login</h2>
+        <input className="input mb-3" placeholder="Email" onChange={e => setEmail(e.target.value)} />
+        <input className="input mb-4" type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
+        <button className="btn-primary w-full">Login</button>
+      </form>
+    </motion.div>
+  );
+}
